3c9c6c25c4449b6720b28ea4be2a54298b92e8bc
Yuva Athur <yuvaraj.a.r@gmail.com>
2019-08-13 23:50:17 -0700

    Got basic flows working with SQLAlchemy

---
+++ SeeCow/seecow/dashboard.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+import json,sys
+from flask_restful import Resource, Api
+import math
+from seecow.dbconn import get_db
+
+
+bp = Blueprint('dashboard', __name__)
+
+@bp.route('/dashboard')
+def dashboard():
+    return render_template('dashboard/overview.html')
